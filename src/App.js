650745a8612b1922997d4b285eb817eb32ce805b
figueroaj1 <42773046+figueroaj1@users.noreply.github.com>
2021-11-23 12:31:58 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css'
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Motion Tracking
+        </p>
+        
+      </header>
+      <body>
+        <button>Yoga Pose 1</button>
+        <button>task2</button>
+        <br />
+        <button>task3</button>
+        <button>task4</button>
+        <br />
+        <button>task5</button>
+        <button>task6</button>
+      </body>
+    </div>
+  );
+}
+
+
+
+export default App;
